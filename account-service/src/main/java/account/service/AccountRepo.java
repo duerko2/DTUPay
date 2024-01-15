a10c2b914b4c462f012cc1266ef416304813163d
marcusjacobsen <S215799@dtu.dk>
2024-01-15 09:21:10 +0100

    account repo and todo

--- account-service/src/main/java/account/service/AccountRepo.java
+++ account-service/src/main/java/account/service/AccountRepo.java
@@ -4,19 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AccountRepo {
-    Map<String, Account> accounts = new HashMap<>();
+    Map<String, Account> accountIdToAccountMap = new HashMap<>();
+    Map<String, String> cprToAccountIdMap = new HashMap<>();
+
 
     public void storeAccount(Account account) {
-        accounts.put(account.getAccountId(), account);
+        accountIdToAccountMap.put(account.getAccountId(), account);
+        cprToAccountIdMap.put(account.getCpr(), account.getAccountId());
     }
     public Account getAccount(String accountId) {
-        return accounts.get(accountId);
+        return accountIdToAccountMap.get(accountId);
     }
     public void deleteAccount(String accountId) {
-        accounts.remove(accountId);
+        Account a = accountIdToAccountMap.get(accountId);
+        if(a==null){
+            return;
+        }
+        cprToAccountIdMap.remove(a.getCpr());
+        accountIdToAccountMap.remove(accountId);
     }
 
     public boolean accountExists(String cpr) {
-        return accounts.values().stream().anyMatch(a -> a.getCpr().equals(cpr));
+        return cprToAccountIdMap.containsKey(cpr);
     }
 }
